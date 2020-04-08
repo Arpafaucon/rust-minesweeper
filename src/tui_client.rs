0e42b853a2f413d47f43a72299a2c8e37e277133
G. Roussel <arpafaucon@gmail.com>
2020-04-08 20:03:20 +0200

    draw box

--- src/tui_client.rs
+++ src/tui_client.rs
@@ -6,14 +6,18 @@ use minefield::field::Cell;
 use std::io;
 use std::str;
 
+use std::iter;
 use termion::{clear, color, cursor, style};
+use std::convert::TryInto;
+use std::usize;
 
-use std::io::{Write};
+use std::io::Write;
 use termion::event::{Event, Key, MouseEvent};
 use termion::input::{MouseTerminal, TermRead};
 use termion::raw::IntoRawMode;
 
-const GRID_OFFSET: (u16,u16) = (3, 3);
+const GRID_OFFSET: (u16, u16) = (3, 3);
+const BOX_CHARS: [char; 10] = ['+', '-', '+', '|', '━', '┃', '┏', '┓', '┗', '┛'];
 
 pub struct TuiClient {
     current_cursor: (usize, usize),
@@ -30,12 +34,32 @@ impl TuiClient {
         }
     }
 
+    fn draw_box<T>(stdout: &mut T, origin_x: u16, origin_y: u16, height: u16, width: u16)
+    where
+        T: std::io::Write,
+    {
+        assert!(height >= 2 && width >= 2);
+        let h_line_width: usize = (width-2).try_into().unwrap();
+        let v_line_height: usize = (height-2).try_into().unwrap();
+        write!(
+            stdout,
+            "{pos}{ul_corner}{u_line}{ur_corner}",
+            pos = cursor::Goto(origin_y, origin_x),
+            ul_corner = BOX_CHARS[6],
+            ur_corner = BOX_CHARS[7],
+            u_line = String::from(BOX_CHARS[5]).repeat(h_line_width);
+        ).unwrap();
+    }
+
     pub fn mainloop(&self) {
         // Get the standard input stream.
         let stdin = std::io::stdin();
         // Get the standard output stream and go to raw mode.
-        let mut stdout = MouseTerminal::from(std::io::stdout()).into_raw_mode().unwrap();
+        let mut stdout = MouseTerminal::from(std::io::stdout())
+            .into_raw_mode()
+            .unwrap();
         let mut cursor = (4u16, 4u16);
+        let grid_shape = self.client.get_state().shape();
 
         write!(
             stdout,
@@ -43,7 +67,8 @@ impl TuiClient {
             // Clear the screen.
             termion::clear::All,
             termion::cursor::Goto(1, 1),
-        ).unwrap();
+        )
+        .unwrap();
         stdout.flush().unwrap();
         for c in stdin.events() {
             match c.unwrap() {
@@ -59,8 +84,8 @@ impl TuiClient {
             write!(
                 stdout,
                 "{clear}{pos}row={row}, col={col}",
-                clear= clear::CurrentLine,
-                pos = cursor::Goto(1,1),
+                clear = clear::CurrentLine,
+                pos = cursor::Goto(1, 1),
                 row = cursor.1,
                 col = cursor.0
             )
